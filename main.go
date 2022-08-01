e1392681191c84729e3e6cc86d21892324fb1044
teesitee <sit_teesitee@hotmail.com>
2022-08-01 23:32:07 +0700

    revise for duplicate in slice and non palin middle

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	s := "xxxxxxxxx"
+	s := "fffff"
 	var palin []string
+	var truePalin []string
+	var finalPalin []string
 	if palin == nil {
 		var countLetter int
 		for i := range s {
@@ -14,22 +19,20 @@ func main() {
 			} else {
 				break
 			}
-
 		}
 		if countLetter == len(s) {
 			fmt.Println("palindrome :", string(s))
 			return
 		}
-
 	}
-
 	if len(s) == 1 {
 		palin = append(palin, s)
 		fmt.Println(palin)
 	} else if len(s) == 0 {
 		palin = append(palin, "")
 		fmt.Println(palin)
-	} else {
+	}
+	if len(s) != 1 || len(s) != 0 {
 		for i := 0; i < (len(s) - 1); i++ {
 			fLetter := string(s[i])
 			var sLetter string
@@ -37,17 +40,37 @@ func main() {
 				sLetter = string(s[j])
 				if fLetter == sLetter {
 					palin = append(palin, string(s[i:j+1]))
-					break
 				}
-
 			}
-
 		}
+		for i := range palin {
+			var reversePal []string
+			pal := palin[i]
+			for j := range pal {
+				reversePal = append(reversePal, string(pal[len(pal)-1-j]))
+			}
+			// fmt.Println("reversePal :", reversePal)
+			joinPal := strings.Join(reversePal, "")
 
+			if pal == joinPal {
+				truePalin = append(truePalin, pal)
+				// fmt.Println("focus", truePalin)
+			}
+			// fmt.Println("truePalin step", i, truePalin)
+		}
+		// fmt.Println("truePalin :", truePalin)
+		checkPalin := make(map[string]bool)
+		for _, item := range truePalin {
+			if _, value := checkPalin[item]; !value {
+				checkPalin[item] = true /*ใส่ true ก็ได้ false ก็ได้ เพราะแค่เช็คว่ามีค่า bool หรือไม่*/
+				finalPalin = append(finalPalin, item)
+			}
+		}
+		// fmt.Println("finalPalin :", finalPalin)
 	}
-	if len(palin) == 0 {
+	if len(truePalin) == 0 {
 		fmt.Println("palindrome :", string(s[0]))
 	} else {
-		fmt.Println("palindrome total :", palin, "\npalindrome :", palin[0])
+		fmt.Println("final palindrome :", finalPalin)
 	}
 }
